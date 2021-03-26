4c075fac975a3c79486d54953223f4aeb19521f2
eldar258 <9eldik7@gmail.com>
2021-03-26 15:56:48 +0300

    При нажатии в любое место карты герой бежит туда

---
+++ Heroes and Towers/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    public LayerMask lm;
+    private float maxDistandceOfRay = 100;
+    private NavMeshAgent myAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo, maxDistandceOfRay, lm))
+            {
+                myAgent.SetDestination(hitInfo.point);
+            }
+        }    
+    }
+}
